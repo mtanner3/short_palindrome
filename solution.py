570365bb0aabe1997ae24e137a3deea680b5b142
Matt Tanner <mtanner3@gmail.com>
2016-08-01 00:56:46 -0600

    Very basic start on the code. Basic outer loop code done. Finds outer matching characters. Within these boundaries need to find inner character pairs.

---
+++ solution.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+
+def findall(target_char, string):
+    positions = []
+    curpos = 0
+    for char in list(string):
+        if char == target_char:
+            positions.append(curpos)
+        curpos += 1
+    return positions
+
+def main(string):
+    for idx1 in range(len(string)-3):
+        char1 = string[idx1]
+        print "starting with %s at %d" % (char1, idx1)
+        others = findall(char1, string[idx1+3:])
+        for idx2 in others: 
+            print "  found at %d as well" % (idx1 + idx2 + 3)
+        
+if __name__ == "__main__":
+    string = raw_input().strip()
+    print string
+    main(string)
